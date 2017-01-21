dd405dcbe96a0cef5a4e343ed4c34521e7771a72
Curt Hill <nwres1git config --global user.email nwres1@yahoo.comgit config --global user.email nwres1>
2017-01-20 19:39:27 -0800

    working on the shell

---
+++ js/spa.shell.js
@@ -0,0 +1,45 @@
+/*
+* spa.shell.js
+* Shell module for SP
+*/
+
+//----------------------- BEGIN MODULE SCOPE VARIABLES -----------------------
+spa.shell = (function () {
+var
+    configMap = {
+        main_html: String()
+            + '<div class="spa-shell-head">'
+            + '<div class="spa-shell-head-logo"></div>'
+            + '<div class="spa-shell-head-acct"></div>'
+            + '<div class="spa-shell-head-search"></div>'
+        + '</div>'
+        + '<div class="spa-shell-main">'
+            + '<div class="spa-shell-main-nav"></div>'
+            + '<div class="spa-shell-main-content"></div>'
+        + '</div>'
+        + '<div class="spa-shell-foot"></div>'
+        + '<div class="spa-shell-chat"></div>'
+        + '<div class="spa-shell-modal"></div>'
+    },
+    stateMap = { $container: null },
+    jqueryMap = {},
+    setJqueryMap, initModule;
+    //----------------------- END MODULE SCOPE VARIABLES -----------------------
+
+    //----------------------- BEGIN UTILITY METHODS -----------------------
+    //----------------------- END UTILITY METHODS  -----------------------
+
+    //----------------------- BEGIN DOM METHODS -----------------------
+    //----------------------- END DOM METHODS  -----------------------
+
+    //----------------------- BEGIN EVENT HANDLERS -----------------------
+    //----------------------- END EVENT HANDLERS -----------------------
+
+    //----------------------- BEGIN PUBLIC METHODS -----------------------
+    //----------------------- END PUBLIC METHODS  -----------------------
+
+    //----------------------- BEGIN UTILITY METHODS -----------------------
+    //----------------------- END UTILITY METHODS  -----------------------
+
+
+}());
