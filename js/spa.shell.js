bd4d97e30c45a420c393d354cf7a4826c0094768
Curt Hill <nwres1git config --global user.email nwres1@yahoo.comgit config --global user.email nwres1>
2017-01-23 19:04:07 -0800

    toggleChat function

--- js/spa.shell.js
+++ js/spa.shell.js
@@ -33,8 +33,44 @@ var
     // Begin DOM method /setJqueryMap/
     setJqueryMap = function () {
         var $container = stateMap.$container;
-        jqueryMap = { $container: $container };
+        jqueryMap = {
+            $container: $container,
+            $chat: $container.find( '.spa-shell-chat')
+        };
     };
+    // End DOM method /setJqueryMap/
+
+    // Begin DOM Method toggleChat/
+    // Purpose : Extends or retracts chat slider
+    // Arguments    :
+    //      * do_extend     - if true, extends slider; if false retracts
+    //      * callback      - optional function to execute at end of animation
+    // Settings     : 
+    //      * chat_extend_time, chat_retract_time
+    //      * chat_extend_height, chat_retract_height
+    // Returns      : Boolean
+    //      * true          - slider animation activated
+    //      * false         - slider animation not activated
+    toggleChat = function ( do_extend, callback ) {
+        var
+            px_chat_ht  = jqueryMap.$chat.height(),
+            is_open     = px_chat_ht === configMap.chat_extend_height,
+            is_closed   = px_chat_ht === configMap.chat_retract_height,
+            is_sliding  = ! is_open && ! is_closed;
+        
+        // avoid race conditions
+        if ( is_sliding ) { return false; }
+
+        // Begin extend chat slider
+        if ( do_extend )  ..........CONTINUE HERE CONTINUER ICI
+            
+
+    };
+
+
+
+
+
     //----------------------- END DOM METHODS  -----------------------
 
     //----------------------- BEGIN EVENT HANDLERS -----------------------
