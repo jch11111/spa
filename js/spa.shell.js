2177458464ec86803474d5d059a6c9b0f783c3e0
Curt Hill <nwres1git config --global user.email nwres1@yahoo.comgit config --global user.email nwres1>
2017-01-27 19:08:53 -0800

    updating anchor when chat slides

--- js/spa.shell.js
+++ js/spa.shell.js
@@ -109,7 +109,12 @@ var
 
     //----------------------- BEGIN EVENT HANDLERS -----------------------
     onClickChat = function ( event ) {
-        toggleChat ( stateMap.is_chat_retracted );
+        if ( toggleChat ( stateMap.is_chat_retracted ) ) {
+            $.uriAnchor.setAnchor( {
+                chat: ( stateMap.is_chat_retracted ? 'open' : 'closed' )
+            } );
+        }
+        
         return false;
     }
     //----------------------- END EVENT HANDLERS -----------------------
