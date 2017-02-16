99f46d0732aaa219c970947ef3dec2222f9848c6
Curt Hill <nwres1git config --global user.email nwres1@yahoo.comgit config --global user.email nwres1>
2017-02-15 19:31:27 -0800

    working on chat module

--- js/spa.chat.js
+++ js/spa.chat.js
@@ -31,6 +31,46 @@ setJqueryMap = function () {
 //------------------------ END DOM METHODS -------------------------
 //------------------------ BEGIN EVENT HANDLERS -------------------------
 //------------------------ END EVENT HANDLERS -------------------------
+//------------------------ BEGIN PUBLIC METHOD -------------------------
+// Begin public method /configModule/
+// Purpose      : Adjust configuration of allowed keyes
+// Arguments    : A map of settable keys and values
+//  * color name - color to use
+// Settings     :
+//  * configMap.settable_map declares allowed keyes
+// Returns      : true
+// Throws       : none
+//
+configModule = function ( input_map ) {
+    spa.util.setConfigMap({
+        input_map       : input_map,
+        settable_map    : configMap.settable_map,
+        config_map      : configMap 
+    });
+    return true;
+};
+// End public method /configModule/
+
+// Begin public method /initModule/
+// Purpose      : Initialize module
+// Arguments    : 
+//  * $container the jquery element used by this feature
+// Returns      : true
+// Throws       : none
+initModule = function ( $container ) {
+    $container.html(configMap.main_html);
+    stateMap.$container = $container;
+    setJqueryMap();
+    return true;
+};
+// End public method /initModule/
+
+// return public methods
+return {
+    configModule    : configModule,
+    initModule      : initModule
+};
+//------------------------ END PUBLIC METHOD -------------------------
 
 
 }());
