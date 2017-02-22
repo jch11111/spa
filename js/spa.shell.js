be15d7f02340e4369b913955ac9d86ab8502fb29
Curt Hill <nwres1git config --global user.email nwres1@yahoo.comgit config --global user.email nwres1>
2017-02-21 18:27:03 -0800

    shell updated to initialize chat

--- js/spa.shell.js
+++ js/spa.shell.js
@@ -266,6 +266,10 @@ var
             schema_map : configMap.anchor_schema_map
         });
 
+        // configure and initialize feature modules
+        spa.chat.configModule( {} );
+        spa.chat.initModule( jqueryMap.$chat );
+
         // Handle URI anchor change events
         // This is done /after/ all feature modules are configured
         // and initiated, otherwise they will not be ready to handle
