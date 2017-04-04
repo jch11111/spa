4e7909a63bca8bfb6c246f6ef1d83021daf159e4
Curt Hill <nwres1git config --global user.email nwres1@yahoo.comgit config --global user.email nwres1>
2017-04-03 19:13:15 -0700

    making progress on spa.chat.js

--- js/spa.chat.js
+++ js/spa.chat.js
@@ -49,10 +49,13 @@ spa.chat = (function () {
 
           slider_open_time: 250,
           slider_close_time: 250,
-          slider_opened_em: 16,
+          slider_opened_em: 18,
           slider_closed_em: 2,
           slider_opened_title: 'Click to close',
           slider_closed_title: 'Click to open',
+
+          slider_opened_min_em: 10,
+          window_height_min_em: 20,
 
           chat_model: null,
           people_model: null,
@@ -102,9 +105,17 @@ spa.chat = (function () {
 
     // Begin DOM method /setPxSizes/
     setPxSizes = function () {
-        var px_per_em, opened_height_em;
+        var px_per_em, window_height_em, opened_height_em;
 
         px_per_em = getEmSize(jqueryMap.$slider.get(0));
+        window_height_em = Math.floor(
+            ( $(window).height() / px_per_em ) + 0.5
+        );
+
+        opened_height_em
+            = window_height_em > configMap.window_height_min_em
+            ? configMap.slider_opened_em
+            : configMap.slider_opened_min_em;
 
         opened_height_em = configMap.slider_opened_em;
 
