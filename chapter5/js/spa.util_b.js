c58ceb67c88202c8a61d49ce7f74368f27e67b25
Curt Hill <nwres1git config --global user.email nwres1@yahoo.comgit config --global user.email nwres1>
2017-04-30 11:57:46 -0700

    spa.util_b.js

--- chapter5/js/spa.util_b.js
+++ chapter5/js/spa.util_b.js
@@ -10,7 +10,7 @@ spa.util_b = (function () {
     var
         configMap = {
             regex_encode_html   : /[&"'><]/g,
-            regex_encode_map    : /["'><]/g,
+            regex_encode_noamp    : /["'><]/g,
             html_encode_map     : {
                 '&'     : '&#38;',
                 '"'     : '&#34;',
@@ -34,8 +34,46 @@ spa.util_b = (function () {
     // Decodes HTML entitites in a browser friendly way
     // See https://stackoverflow.com/questions/1912501/unescape-html-entities-in-javascript
     decodeHtml = function ( str ) {
-
+        return $('<div/>').html(str || '').text();
     };
+    // End decodeHtml
 
+    // Begin encodeHtml
+    // This is a single pass encoder for html entitiies and handles an arbitrary number of characters
+    encodeHtml = function ( input_arg_str, exclude_amp ) {
+        var
+            input_str = String(input_arg_str),
+            regex,
+            lookup_map;
 
+        if (exclude_amp) {
+            lookup_map = configMap.encode_noamp_map;
+            regex = configMap.regex_encode_noamp;
+        } else {
+            lookup_map = configMap.html_encode_map;
+            regex = configMap.regex_encode_html;
+        }
+        return input_str.replace(regex, function ( match, name ) {
+            retrun lookup_map[match] || '';
+        });
+    };
+    // End encodeHtml
+
+    // Begin getEmSize
+    // returns size of em in pixels
+    getEmSize = function ( elem ) {
+        return Number(
+            getComputedStyle( elem, '').fontSize.match(/\d*\.?\d*/)[0]
+        );
+    }
+    // End getEmSize
+
+    // export methods
+    return {
+        decodeHtml  : decodeHtml,
+        encodeHtml  : encodeHtml,
+        getEmSize   : getEmSize
+    };
+    //------------------------ End public methods---------------------------------
+    
 } ());
