09066695612077a2979d6ac0dd38907c53132d87
Curt Hill <nwres1git config --global user.email nwres1@yahoo.comgit config --global user.email nwres1>
2017-06-12 19:07:09 -0700

    fixed bug

--- chapter5/js/spa.fake.js
+++ chapter5/js/spa.fake.js
@@ -8,8 +8,7 @@ spa.fake = (function () {
     var getPeopleList;
 
     getPeopleList = function () {
-        return 
-            [
+        return [
                 {name: 'Betty',     _id: 'id_01', css_map: {top: 20,    left: 20,   'background-color': 'rgb(128, 128, 128)'} },
                 {name: 'Mike',      _id: 'id_02', css_map: {top: 60,    left: 20,   'background-color': 'rgb(128, 255, 128)'} },
                 {name: 'Pebbles',   _id: 'id_03', css_map: {top: 100,   left: 20,   'background-color': 'rgb(128, 192, 192)'} },
