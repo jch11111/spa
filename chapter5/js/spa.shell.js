c0bc43fa6381638c3dd8db419d289aca05b0c036
Curt Hill <nwres1git config --global user.email nwres1@yahoo.comgit config --global user.email nwres1>
2017-07-05 18:38:19 -0700

    login/logout

--- chapter5/js/spa.shell.js
+++ chapter5/js/spa.shell.js
@@ -12,6 +12,7 @@
 /*global $, spa */
 
 spa.shell = (function () {
+    'use strict';
     //---------------- BEGIN MODULE SCOPE VARIABLES --------------
     var
       configMap = {
@@ -21,9 +22,12 @@ spa.shell = (function () {
           resize_interval : 200, //milliseconds interval for throttling resize
           main_html: String()
             + '<div class="spa-shell-head">'
-              + '<div class="spa-shell-head-logo"></div>'
+              + '<div class="spa-shell-head-logo">'
+                 + '<h1>SPA</h1>'
+                 + '<p>javascript end to end</p>'
+              + '</div>'
               + '<div class="spa-shell-head-acct"></div>'
-              + '<div class="spa-shell-head-search"></div>'
+              //+ '<div class="spa-shell-head-search"></div>'
             + '</div>'
             + '<div class="spa-shell-main">'
               + '<div class="spa-shell-main-nav"></div>'
@@ -39,8 +43,9 @@ spa.shell = (function () {
       },
       jqueryMap = {},
 
-      copyAnchorMap, setJqueryMap,
-      changeAnchorPart, onHashchange, onResize,
+      copyAnchorMap, setJqueryMap, changeAnchorPart, 
+      onResize, onHashchange,
+      onTapAcct, onLogin, onLogout,
       setChatAnchor, initModule;
     //----------------- END MODULE SCOPE VARIABLES ---------------
 
@@ -55,9 +60,34 @@ spa.shell = (function () {
     // Begin DOM method /setJqueryMap/
     setJqueryMap = function () {
         var $container = stateMap.$container;
-        jqueryMap = { $container: $container };
+        jqueryMap = { 
+            $container  : $container,
+            $acct       : $container.find('.spa-shell-head-acct'),
+            $nav        : $container.find('.spa-shell-main-nav')
+        };
     };
     // End DOM method /setJqueryMap/
+
+    onTapAcct = function (event) {
+        var acct_text, user_name, user = spa.model.people.get_user();
+
+        if ( user.get_is_anon() ) {
+            user_name = prompt('Please sign in');
+            spa.model.people.login( user_name );
+            jqueryMap.$acct.text( '...processing...');
+        } else {
+            spa.model.people.logout();
+        }
+        return false;
+    };
+
+    onLogin = function ( event, login_user ) {
+        jqueryMap.$acct.text( login_user.name );
+    };
+
+    onLogout = function ( event, logout_user ) {
+        jqueryMap.$acct.text( 'Please sign-in');
+    };
 
     // Begin DOM method /changeAnchorPart/
     // Purpose    : Changes part of the URI anchor component
@@ -274,6 +304,12 @@ spa.shell = (function () {
           .bind('hashchange', onHashchange)
           .trigger('hashchange');
 
+        $.gevent.subscribe( $container, 'spa-login', onLogin );
+        $.gevent.subscribe( $container, 'spa-logout', onLogout );
+
+        jqueryMap.$acct
+            .text( 'Please sign-in' )
+            .bind( 'utap', onTapAcct );
     };
     // End PUBLIC method /initModule/
 
